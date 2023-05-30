211e8576a8edf1e7c270dfa557b7ac80e057571a
Thibaut DE STEFANI <thibaut.destefani@gmail.com>
2023-05-30 10:28:41 +0200

    avancement projet

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :houses
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :houses do
+    member do
+      resources :bookings, only: %i[new create]
+    end
+  end
 end
