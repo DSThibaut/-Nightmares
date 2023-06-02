58b13c722ad58a6e2d8525023eeb121a1afa41b1
Thibaut DE STEFANI <thibaut.destefani@gmail.com>
2023-06-02 11:11:57 +0200

    map edit

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -5,6 +5,7 @@ class HousesController < ApplicationController
       {
         lat: house.latitude,
         lng: house.longitude
+        info_window_html: render_to_string(partial: "info_window", locals: {house: house})
       }
     end
   end
